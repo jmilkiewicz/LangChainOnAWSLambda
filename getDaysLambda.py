251ddf8e67c1b3fa16853dd82997bf857f6da202
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2024-08-09 15:31:41 +0200

    changing to check next 21 days instead of 30

--- getDaysLambda.py
+++ getDaysLambda.py
@@ -29,7 +29,7 @@ def handler(event, context):
 
     model = buildLLM(key=getApiKey(), temperature=0.0)
 
-    days = findRelevantDays(model, datetime.now(), datetime.now() + timedelta(days=30))
+    days = findRelevantDays(model, datetime.now(), datetime.now() + timedelta(days=21))
 
     dicts = [{"event": relevantDay.dict()} | {"index": index} for index, relevantDay in enumerate(days)]
 
