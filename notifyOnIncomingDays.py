c329f31bb92ed3a8a400c2738b0ba07b9707d6e6
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2024-08-09 15:23:42 +0200

    sending emails even when no events coming

--- notifyOnIncomingDays.py
+++ notifyOnIncomingDays.py
@@ -1,6 +1,7 @@
-from sendEmail import send_email
 import chevron
+
 from findRelevantDaysFromCalendar import findRelevantDays
+from sendEmail import send_email
 
 
 def buildHtmlEmailBody(days):
@@ -11,17 +12,33 @@ def buildTxtEmailBody(days):
     return fromTemplate(days, "email-txt.mustache")
 
 
-def fromTemplate(days, tmplFile='email.mustache'):
+def fromTemplate(model, tmplFile='email.mustache'):
     with open('templates/%s' % tmplFile, 'r') as f:
-        return chevron.render(f, days)
+        return chevron.render(f, model)
+
+
+def buildTxtNoEventsEmailBody(fromD, toDateD):
+    return fromTemplate({'from': fromD, 'to': toDateD}, "email-no-events-txt.mustache")
+
+
+def buildHtmlNoEventsEmailBody(fromD, toDateD):
+    return fromTemplate({'from': fromD, 'to': toDateD}, "email-no-events-html.mustache")
 
 
 def doNotify(model, fromDate, toDate):
+    fromD = fromDate.strftime("%d, %b %Y")
+    toDateD = toDate.strftime("%d, %b %Y")
     days = findRelevantDays(model, fromDate, toDate)
+
+    subject = f'Nadchodzące ({fromD} - {toDateD}) eventy do ogłoszenia na instagram'
     if days:
         daysDict = [relevantDay.dict() for relevantDay in days]
-        send_email(subject="Nadchodzące eventy do ogłoszenia na instagram", body_html=buildHtmlEmailBody(daysDict),
+        send_email(subject=subject, body_html=buildHtmlEmailBody(daysDict),
                    body_txt=buildTxtEmailBody(daysDict),
                    to="jmilkiewicz@gmail.com")
     else:
-        print(f'nie ma eventów {fromDate.strftime("%d, %b %Y")} - {fromDate.strftime("%d, %b %Y")}')
+
+        print(f'nie ma eventów {fromD} - {toDateD}')
+        send_email(subject=subject, body_html=buildHtmlNoEventsEmailBody(fromD, toDateD),
+                   body_txt=buildTxtNoEventsEmailBody(fromD, toDateD),
+                   to="jmilkiewicz@gmail.com")
